597f28408755d6a6403fb1dea75f69648d79fc5e
tayu14 <tayu14x@gmail.com>
2021-01-21 11:43:41 +0900

    テストコードまでの機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         validates :email, uniqueness: true
+
+         validates :nick_name,          presence: true
+         validates :first_name,         presence: true
+         validates :family_name,        presence: true
+         validates :first_name_kana,    presence: true
+         validates :family_name_kana,   presence: true
+         validates :birthday,           presence: true
+
+          has_many :items
+          has_many :orders
+
 end
