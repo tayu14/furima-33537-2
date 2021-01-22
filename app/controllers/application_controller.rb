a6499060bc07418b088aa2bdaf45ec9cf0b3ef3c
tayu14 <76463528+tayu14@users.noreply.github.com>
2021-01-22 12:43:57 +0900

    Revert "ユーザー管理機能２"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name , :first_name , :family_name , :first_name_kana , :family_name_kana , :birthday])
-  end
 end
