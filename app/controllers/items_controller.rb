5e08dfcda367ceef4468bfc04d74bec723758c04
tayu14 <tayu14x@gmail.com>
2021-01-29 14:56:15 +0900

    商品一覧表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [ :index]
+  def index
+    @item = Item.all
+  end
   def new
     @item = Item.new
   end
